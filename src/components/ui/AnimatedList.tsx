96699ff6a0473c44938432b6d05cd44bc594a725
LokeshXs <lokesh1129ece@gmail.com>
2025-10-02 18:41:18 +0000

    resolved the type erro

--- src/components/ui/AnimatedList.tsx
+++ src/components/ui/AnimatedList.tsx
@@ -24,7 +24,7 @@ export const AnimatedList = React.memo(
 
     const itemsToShow = useMemo(
       () => childrenArray.slice(0, index + 1).reverse(),
-      [index, childrenArray],
+      [index, childrenArray]
     );
 
     return (
@@ -38,21 +38,21 @@ export const AnimatedList = React.memo(
         </AnimatePresence>
       </div>
     );
-  },
+  }
 );
 
 AnimatedList.displayName = "AnimatedList";
 
 export function AnimatedListItem({ children }: { children: React.ReactNode }) {
-  const animations = {
-    initial: { scale: 0, opacity: 0 },
-    animate: { scale: 1, opacity: 1, originY: 0 },
-    exit: { scale: 0, opacity: 0 },
-    transition: { type: "spring", stiffness: 350, damping: 40 },
-  };
-
   return (
-    <motion.div {...animations} layout className="mx-auto w-full">
+    <motion.div
+      initial={{ scale: 0, opacity: 0 }}
+      animate={{ scale: 1, opacity: 1, originY: 0 }}
+      exit={{ scale: 0, opacity: 0 }}
+      transition={{ type: "spring", stiffness: 350, damping: 40 }}
+      layout
+      className="mx-auto w-full"
+    >
       {children}
     </motion.div>
   );
